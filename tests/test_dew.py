88f3fd771bd53c79bb9c62e999db1c3f9220cf43
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-02-24 10:50:07 -0700

    Move test_dew.py to tests folder

--- tests/test_dew.py
+++ tests/test_dew.py
@@ -17,20 +17,20 @@ from ditto.readers.dew.read import reader
 from ditto.store import Store
 from ditto.writers.opendss.write import writer
 
+
 def test_dew_reader_writer():
     # TODO: Remove hardcoded paths
-    dew_models_dir = os.path.join('C:/Users/athakall/Desktop/DiTTo-develop - Copy/ditto/readers/DEW/Models')
-    dew_database_dir = os.path.join('C:/Users/athakall/Desktop/DiTTo-develop - Copy/ditto/readers/DEW/DataBase')
+    dew_models_dir = os.path.abspath(os.path.join('../ditto/readers/DEW/Models'))
+    dew_database_dir = os.path.abspath(os.path.join('../ditto/readers/DEW/DataBase'))
 
-    modelfile = 'IEEE-13.dew'  #IEEE 13Bus System
-    #modelfile = 'IEEE-123.dew' #IEEE 123Bus System
+    modelfile = 'IEEE-13.dew' #IEEE 13Bus System
 
-    #database read
+    # database read
     databasename = 'DataBase.xlsx'
-    inputfile = open(os.path.join(dew_models_dir,modelfile),'r')
-    databasepath = os.path.join(dew_database_dir,databasename)
+    inputfile = open(os.path.join(dew_models_dir, modelfile), 'r')
+    databasepath = os.path.join(dew_database_dir, databasename)
     m = Store()
     reader = reader()
     reader.parse(m, inputfile, databasepath)
-    writer=writer(linecodes_flag=True, output_path='C:/Users/athakall/Desktop/DiTTo-develop - Copy/ditto/readers/DEW/Models/IEEE_13_node_output/')
+    writer = writer(linecodes_flag=True, output_path=os.path.abspath('../ditto/readers/DEW/Models/IEEE_13_node_output/'))
     writer.write(m, verbose=True)
