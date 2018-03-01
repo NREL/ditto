03fc5cf665a60631fa1099dcfc373479a5df979c
ngensoll <nicolas.gensollen@nrel.gov>
2018-03-01 11:22:57 -0700

    Update reader testing

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -22,3 +22,41 @@ def test_gld_reader():
         r = reader()
         r.parse(m, os.path.join(gridlabd_models_dir,modelfile))
 
+
+def test_cyme_reader():
+	'''
+	TODO
+	'''
+	from ditto.readers.cyme.read import reader 
+	from ditto.store import Store 
+	cyme_models=[f for f in os.listdir('./data/cyme/') if not f.startswith('.')]
+	for model in cyme_models:
+		m = Store()
+		r = reader(data_folder_path=os.path.join('./data/cyme',model))
+		r.parse(m)
+		#TODO: Log properly
+		print('>Cyme model {model} parsed.\n'.format(model=model)) 
+
+
+def test_opendss_reader():
+	'''
+	TODO
+	'''
+	from ditto.readers.opendss.read import reader
+	from ditto.store import Store 
+	opendss_models=[f for f in os.listdir('./data/opendss/') if not f.startswith('.')]
+	for model in opendss_models:
+		m = Store()
+		r = reader(master_file=os.path.join('./data/opendss',model,'master.dss'),
+			       buscoordinates_file=os.path.join('./data/opendss',model,'buscoord.dss'))
+		r.parse(m)
+		#TODO: Log properly
+		print('>OpenDSS model {model} parsed.\n'.format(model=model))
+
+
+def test_dew_reader():
+	'''
+	TODO
+	'''
+	pass
+
