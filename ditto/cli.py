b5a2ddfc9a0bfb2304c8a32d70ab08452ced8cc4
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-03-22 10:20:37 -0600

    Add stub for cli

--- ditto/cli.py
+++ ditto/cli.py
@@ -5,6 +5,7 @@ from builtins import super, range, zip, round, map
 import click
 import os
 from . import version
+from .readers.readers import registered_readers
 
 
 @click.group()
@@ -14,10 +15,17 @@ def cli():
 
 
 @cli.command()
+@click.option("--input", type=click.Path(exists=True), help="Path to input file")
+@click.option("--output", type=click.Path(), help="Path to output file")
+@click.option("--from", help="Convert from OpenDSS, Cyme, GridLAB-D")
+@click.option("--to", help="Convert to OpenDSS, Cyme, GridLAB-D")
 def convert(**kwargs):
     """ Convert from one type to another"""
+    print(kwargs)
+    if not kwargs["from"] in registered_readers.keys():
+        raise NotImplementedError("Cannot read from format '{}'".format(kwargs["from"]))
 
-    pass
+
 
 
 if __name__ == "__main__":
