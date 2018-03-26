f61c813e4beadbd8d5c85470356abee46d3b1e31
ngensoll <nicolas.gensollen@nrel.gov>
2018-03-26 16:15:54 -0600

    Add test_opendss_to_gridlabd

---
+++ tests/test_opendss_to_gridlabd.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+test_opendss_to_gridlabd
+----------------------------------
+
+Tests for OpenDSS --> GridlabD conversion
+"""
+import os
+import pytest as pt
+
+current_directory = os.path.realpath(os.path.dirname(__file__))
+
+def test_opendss_to_gridlabd():
+    '''
+        Test the OpenDSS to GridlabD conversion.
+    '''
+    from ditto.readers.opendss.read import Reader
+    from ditto.store import Store
+    from ditto.writers.gridlabd.write import Writer
+
+    opendss_models=[f for f in os.listdir(os.path.join(current_directory,'data/opendss/')) if not f.startswith('.')]
+    for model in opendss_models:
+        m = Store()
+        r = Reader(
+            master_file=os.path.join(current_directory, 'data/opendss/{model}/master.dss'.format(model=model)),
+            buscoordinates_file=os.path.join(current_directory, 'data/opendss/{model}/buscoord.dss'.format(model=model))
+        )
+        r.parse(m)
+        m.set_names()
+        #TODO: Log properly
+        print('>OpenDSS model {model} red...'.format(model=model))
+        output_path = os.path.join(current_directory, "./")
+        w = Writer()
+        w.write(m)
+        #TODO: Log properly
+        print('>...and written to GridLabD.\n')
