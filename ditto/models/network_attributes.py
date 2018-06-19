e8cb0d407abdefc4cee0256b8b348b2abee89938
ngensoll <nicolas.gensollen@nrel.gov>
2018-06-19 16:01:44 -0600

    Add NetworkAttributes class to the API

---
+++ ditto/models/network_attributes.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function
+from builtins import super, range, zip, round, map
+
+from .base import DiTToHasTraits, Float, Complex, Unicode, Any, Int, List, observe, Instance
+
+from .position import Position
+
+class NetworkAttributes(DiTToHasTraits):
+    '''Class describing the networks of a system (feeders, substations, subtransmission...).'''
+    name = Unicode(help='''Network ID.''')
+    network_type = Unicode(help='''Type of network (feeder, substation...).''')
+    headnode = Unicode(help='''Name of the headnode/FEEDERHEAD of the network.''', default_value=None)
+    nominal_voltage = Float(help='''Nominal voltage at the feeder head.''', default_value=None)
+    sourceID = Unicode(help='''Name of the Power Source object. Either the connected source or source equivalent.''', default_value=None)
+    substation_name = Unicode(help='''Name of the substation of this network.''', default_value=None)
+    positions = List(Instance(Position), help="""Position of the subnetwork. """, default_value=None)
+    
+    def build(self, model, Asset=None, ConnectivityNode=None, Location=None):
+
+        self._model = model
