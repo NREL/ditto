4a379045972d7df39f59e05b516f87c576499e4e
ngensoll <nicolas.gensollen@nrel.gov>
2018-06-21 17:03:05 -0600

    Add reject_outliers method to utils

--- ditto/utils.py
+++ ditto/utils.py
@@ -3,6 +3,7 @@ from builtins import bytes, str, open, super, range, zip, round, input, int, pow
 
 from json import JSONEncoder
 import logging
+import numpy as np
 
 
 class DittoEncoder(JSONEncoder):
@@ -40,3 +41,16 @@ def start_file_log(
     logfile.setFormatter(logformat)
     logging.getLogger().setLevel(log_level)
     logging.getLogger().addHandler(logfile)
+
+
+def reject_outliers(data, m = 2.):
+    """
+    This function returns a Numpy array where the outliers were removed as well
+    as the indices of the elements to keep and the indices of the elements to remove.
+    The estimator used is the median and absolute distance to the median since
+    the mean of a distribution tends to be biased by outliers.
+    """
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d/mdev if mdev else 0.
+    return data[s<m], np.argwhere(s<m).flatten(), np.argwhere(s>=m).flatten()
