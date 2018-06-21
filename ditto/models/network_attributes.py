038705740249e76d5261e5edb21a123a81f65a63
ngensoll <nicolas.gensollen@nrel.gov>
2018-06-21 17:00:29 -0600

    Update NetworkAttributes class

--- ditto/models/network_attributes.py
+++ ditto/models/network_attributes.py
@@ -13,8 +13,9 @@ class NetworkAttributes(DiTToHasTraits):
     nominal_voltage = Float(help='''Nominal voltage at the feeder head.''', default_value=None)
     sourceID = Unicode(help='''Name of the Power Source object. Either the connected source or source equivalent.''', default_value=None)
     substation_name = Unicode(help='''Name of the substation of this network.''', default_value=None)
-    positions = List(Instance(Position), help="""Position of the subnetwork. """, default_value=None)
-    
+    positions = List(Instance(Position), help="""List of Nodes Positions belonging to the subnetwork. """, default_value=None)
+    average_position = List(Instance(Position), help="""Average position for the subnetwork.""", default_value=None)
+
     def build(self, model, Asset=None, ConnectivityNode=None, Location=None):
 
         self._model = model
