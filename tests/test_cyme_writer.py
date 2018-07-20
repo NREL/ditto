b09afdea825f7b7ca6cf3a89d15ce1a1af49c59d
ngensoll <nicolas.gensollen@nrel.gov>
2018-07-20 14:36:39 -0600

    Add basic tests for writing center tap loads to CYME

---
+++ tests/test_cyme_writer.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+test_cyme_writer
+----------------------------------
+
+Tests for cyme writer
+"""
+import os
+import pytest as pt
+from ditto.store import Store
+from ditto.writers.cyme.write import Writer
+
+current_directory = os.path.realpath(os.path.dirname(__file__))
+
+
+def test_center_tap_load_writing():
+    """
+    Tests the writing of center tap loads.
+    """
+    from ditto.models.load import Load
+    from ditto.models.phase_load import PhaseLoad
+
+    m = Store()
+    l = Load(m)
+    l.name = "load1"
+    l.is_center_tap = 1
+    l.center_tap_perct_1_N = .5
+    l.center_tap_perct_N_2 = .5
+    l.center_tap_perct_1_2 = 0
+
+    pl = PhaseLoad(m)
+    pl.p = 10
+    pl.q = 8
+    pl.phase = "C"
+    l.phase_loads.append(pl)
+
+    w = Writer(output_path=current_directory)
+    w.write(m)
+
+    with open(os.path.join(current_directory, "loads.txt"), "r") as fp:
+        lines = fp.readlines()
+
+    assert (
+        lines[-1] == ",SPOT,0,C,0.01,0.008,0,PQ,50.0,50.0,0.005,0.004,0.005,0.004,0\n"
+    )
+
+    # Cleaning
+    os.remove(os.path.join(current_directory, "loads.txt"))
+    os.remove(os.path.join(current_directory, "equipment.txt"))
+    os.remove(os.path.join(current_directory, "network.txt"))
