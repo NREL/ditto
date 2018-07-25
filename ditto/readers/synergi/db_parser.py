e277446eaf154dd7570c8af9dd960ad4ac3db371
ngensoll <nicolas.gensollen@nrel.gov>
2018-07-25 13:01:01 -0600

    Clean DbParser class.

--- ditto/readers/synergi/db_parser.py
+++ ditto/readers/synergi/db_parser.py
@@ -14,47 +14,46 @@ class DbParser:
     Class implementing the reading of the MDB tables used by Synergi.
     """
 
-    __Paths = {}
-    SynergiDictionary = {}
-
-    def __init__(self, FilesDict):
+    def __init__(self, input_file):
         """
         Class constructor.
         """
-        self.__Paths["Synergi File"] = FilesDict["Synegi Circuit Database"]
-        self.__ParseSynergiDatabase(self.__Paths["Synergi File"])
+        self.SynergiDictionary = {}
+        self.paths = {}
+        self.paths["Synergi File"] = input_file
+        self.ParseSynergiDatabase()
 
-    def __ParseSynergiDatabase(self, dataFile):
+    def ParseSynergiDatabase(self):
         """
         Use Pandas Access to convert the MDB tables to Pandas DataFrames.
         """
-        print("Opening synergie database - ", self.__Paths["Synergi File"])
-        table_list = mdb.list_tables(self.__Paths["Synergi File"])
+        print("Opening synergie database - ", self.paths["Synergi File"])
+        table_list = mdb.list_tables(self.paths["Synergi File"])
         for table in table_list:
             self.SynergiDictionary[table] = mdb.read_table(
-                self.__Paths["Synergi File"], table
+                self.paths["Synergi File"], table
             )
         return
 
-    def __ParseSynergiDatabase_deprecated(self, dataFile):
-        """
-        This only works on Windows.
-        Deprecated.
-        """
-        print("Opening synergie database - ", self.__Paths["Synergi File"])
-        databaseFile = os.getcwd() + "\\" + dataFile
-        connectionString = (
-            "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=%s" % databaseFile
-        )
-        dbConnection = pyodbc.connect(connectionString)
-        cursor = dbConnection.cursor()
-        TableInfo = [[list(row)[2], list(row)[3]] for row in cursor.tables()]
-        for TableName, TableType in TableInfo:
-            if TableType.lower() == "table":
-                print("Parsing table - ", TableName)
-                sql = "select * from " + TableName
-                data = pd.read_sql(sql, dbConnection)
-                self.SynergiDictionary[TableName] = data
-        cursor.close()
-        dbConnection.close()
-        return
+    # def __ParseSynergiDatabase_deprecated(self, dataFile):
+    #     """
+    #     This only works on Windows.
+    #     Deprecated.
+    #     """
+    #     print("Opening synergie database - ", self.__Paths["Synergi File"])
+    #     databaseFile = os.getcwd() + "\\" + dataFile
+    #     connectionString = (
+    #         "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=%s" % databaseFile
+    #     )
+    #     dbConnection = pyodbc.connect(connectionString)
+    #     cursor = dbConnection.cursor()
+    #     TableInfo = [[list(row)[2], list(row)[3]] for row in cursor.tables()]
+    #     for TableName, TableType in TableInfo:
+    #         if TableType.lower() == "table":
+    #             print("Parsing table - ", TableName)
+    #             sql = "select * from " + TableName
+    #             data = pd.read_sql(sql, dbConnection)
+    #             self.SynergiDictionary[TableName] = data
+    #     cursor.close()
+    #     dbConnection.close()
+    #     return
