d964e333e91ad919b651803c09efad8fcf12d2de
ngensoll <nicolas.gensollen@nrel.gov>
2018-08-22 15:21:23 -0600

    Add tests for OverheadLineParser

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -193,3 +193,107 @@ def test_underground_line_parser():
     assert_array_almost_equal(
         ulp.impedance_matrix, kersting_result_impedance_matrix, decimal=4
     )
+
+
+def test_overhead_line_parser():
+    """
+    Test the OverheadLineParser class.
+    """
+    from ditto.parsers.overhead_line_parser import OverheadLineParser
+
+    # Here we use the example for overhead lines from Kersting's book, page 93
+    # We have a 3 phase, 4 wires overhead line:
+    #
+    olp = OverheadLineParser("overhead_test_parser", 3, 4)
+    assert olp.name == "overhead_test_parser"
+    assert olp.n_phase == 3
+    assert olp.n_cond == 4
+
+    # The phase wires are 336,400 26/7 ACSR
+    # and the neutral wire is 4/0 6/1 ACSR
+    #
+    # The GMR of the phase conductors is 0.0244 ft
+    # And the GMR of the neutral conductor is 0.00814 ft
+    olp.set_property("GMR", [0.0244 * ureg.feet] * 3 + [0.00814 * ureg.feet])
+    assert np.all(
+        np.array([x.magnitude for x in olp.GMR]) == np.array([0.0244] * 3 + [0.00814])
+    )
+
+    # The Resistance of the phase conductors is 0.306 Ohm/mile
+    # And the resistance of the neutral conductor is 0.5920 Ohm/mile
+    olp.set_property(
+        "resistance",
+        [0.306 * ureg.parse_expression("ohm/mi")] * 3
+        + [0.5920 * ureg.parse_expression("ohm/mi")],
+    )
+    assert np.all(
+        np.array([x.magnitude for x in olp.resistance])
+        == np.array([0.306] * 3 + [0.5920])
+    )
+
+    # The example gives directly the distances
+    # We still provide the positions
+    olp.set_property(
+        "positions",
+        [
+            (0 * ureg.feet, (25.0 + 4.0) * ureg.feet),
+            (2.5 * ureg.feet, (25.0 + 4.0) * ureg.feet),
+            ((4.5 + 2.5) * ureg.feet, (25.0 + 4.0) * ureg.feet),
+            ((2.5 + (4.5 - 3.0)) * ureg.feet, 25.0 * ureg.feet),
+        ],
+    )
+    # Check the distance values in feet
+    assert olp.distance(olp.positions[0], olp.positions[1], "ft").magnitude == 2.5
+    assert olp.distance(olp.positions[0], olp.positions[2], "ft").magnitude == 7.0
+    assert_almost_equal(
+        olp.distance(olp.positions[0], olp.positions[3], "ft").magnitude,
+        5.6569,
+        decimal=4,
+    )
+    assert olp.distance(olp.positions[1], olp.positions[2], "ft").magnitude == 4.5
+    assert_almost_equal(
+        olp.distance(olp.positions[1], olp.positions[3], "ft").magnitude,
+        4.272,
+        decimal=4,
+    )
+    assert olp.distance(olp.positions[2], olp.positions[3], "ft").magnitude == 5.0
+
+    # We have everything we need to compute the impedance matrix
+    olp.compute_impedance_matrix("ohm/mi")
+
+    # z_ij
+    kersting_result_z_ij = np.array(
+        [
+            [0.4013 + 1.4133j, 0.0953 + 0.8515j, 0.0953 + 0.7266j],
+            [0.0953 + 0.8515j, 0.4013 + 1.4133j, 0.0943 + 0.7802j],
+            [0.0953 + 0.7266j, 0.0953 + 0.7802j, 0.4013 + 1.4133j],
+        ]
+    )
+    assert_array_almost_equal(olp.z_ij, kersting_result_z_ij, decimal=3)
+
+    # z_in
+    kersting_result_z_in = np.array(
+        [[0.0953 + 0.7524j], [0.0953 + 0.7865j], [0.0953 + 0.7674j]]
+    )
+    assert_array_almost_equal(olp.z_in, kersting_result_z_in, decimal=4)
+
+    # z_nn
+    kersting_result_z_nn = np.array([[0.6873 + 1.5465j]])
+    assert_array_almost_equal(olp.z_nn, kersting_result_z_nn, decimal=4)
+
+    # z_nj
+    kersting_result_z_nj = np.array(
+        [[0.0953 + 0.7524j, 0.0953 + 0.7865j, 0.0953 + 0.7674j]]
+    )
+    assert_array_almost_equal(olp.z_nj, kersting_result_z_nj, decimal=4)
+
+    kersting_result_impedance_matrix = np.array(
+        [
+            [0.4576 + 1.0780j, 0.1560 + .5017j, 0.1535 + 0.3849j],
+            [0.1560 + 0.5017j, 0.4666 + 1.0482j, 0.1580 + 0.4236j],
+            [0.1535 + 0.3849j, 0.1580 + 0.4236j, 0.4615 + 1.0651j],
+        ]
+    )
+    assert_array_almost_equal(
+        olp.impedance_matrix, kersting_result_impedance_matrix, decimal=4
+    )
