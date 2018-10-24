90cb01c9033a211576c1e869120b26df659a1367
ngensoll <nicolas.gensollen@nrel.gov>
2018-10-24 11:04:01 -0600

    Small bugfix...

--- ditto/writers/json/write.py
+++ ditto/writers/json/write.py
@@ -256,4 +256,4 @@ class Writer(AbstractWriter):
                 }
 
         with open(os.path.join(self.output_path, self.filename), "w") as f:
-            f.write(json_tricks.dumps(json_dump["model"], allow_nan=True, indent=4))
+            f.write(json_tricks.dumps(json_dump, allow_nan=True, indent=4))
