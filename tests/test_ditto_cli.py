197140f5f47a800b7977e242eed16398d2b087ee
Tarek Elgindy <tarek.elgindy@nrel.gov>
2019-03-17 00:26:56 -0600

    returning test folder to path

--- tests/test_ditto_cli.py
+++ tests/test_ditto_cli.py
@@ -18,7 +18,7 @@ def test_opendss_to_gridlabd_cli():
     print(output_path.name)
     p = subprocess.Popen(
         shlex.split(
-            """ ditto-cli convert --from="opendss" --to="gridlabd" --input="./data/small_cases/opendss/ieee_13node/master.dss" --output="{}" """.format(
+            """ ditto-cli convert --from="opendss" --to="gridlabd" --input="./tests/data/small_cases/opendss/ieee_13node/master.dss" --output="{}" """.format(
                 output_path.name
             ).strip()
         )
@@ -46,7 +46,7 @@ def test_gridlabd_to_opendss_cli():
     output_path = tempfile.TemporaryDirectory()
     p = subprocess.Popen(
         shlex.split(
-            """ ditto-cli convert --from="gridlabd" --to="opendss" --input="./data/small_cases/gridlabd/4node.glm" --output="{}" """.format(
+            """ ditto-cli convert --from="gridlabd" --to="opendss" --input="./tests/data/small_cases/gridlabd/4node.glm" --output="{}" """.format(
                 output_path.name
             ).strip()
         )
@@ -60,7 +60,7 @@ def test_opendss_to_ephasor_cli():
     output_path = tempfile.TemporaryDirectory()
     p = subprocess.Popen(
         shlex.split(
-            """ditto-cli convert --from="opendss" --to="ephasor" --input="./data/small_cases/opendss/ieee_13node/master.dss" --output="{}" """.format(
+            """ditto-cli convert --from="opendss" --to="ephasor" --input="./tests/data/small_cases/opendss/ieee_13node/master.dss" --output="{}" """.format(
                 output_path.name
             ).strip()
         )
