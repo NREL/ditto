43c01aec3b7deac526770c54e11e6d240a539b56
Bhavya Kavuri <kavuri.bhavya@gmail.com>
2019-04-08 12:51:39 -0600

    Fixing tests

--- tests/readers/opendss/Nodes/test_nodes.py
+++ tests/readers/opendss/Nodes/test_nodes.py
@@ -29,31 +29,23 @@ def test_nodes():
     r.parse(m)
     m.set_names()
 
-    assert (m["st_mat"].name) == "st_mat"
-    assert (m["st_mat"].nominal_voltage) == None
-    assert (m["st_mat"].phases) == []
-    assert (m["st_mat"].positions[0].long) == float(200)
-    assert (m["st_mat"].positions[0].lat) == float(400)
-    assert (m["st_mat"].positions[0].elevation) == 0
-    assert (m["st_mat"].feeder_name) == "st_mat_src"
-
     assert (m["bus1"].name) == "bus1"
     assert (m["bus1"].nominal_voltage) == None
     assert (m["bus1"].positions[0].long) == float(300)
     assert (m["bus1"].positions[0].lat) == float(400)
     assert (m["bus1"].positions[0].elevation) == 0
-    assert (m["bus1"].feeder_name) == "st_mat_src"
+    assert (m["bus1"].feeder_name) == "sourcebus_src"
 
     assert (m["sourcebus"].name) == "sourcebus"
     assert (m["sourcebus"].nominal_voltage) == None
     assert (m["sourcebus"].positions[0].long) == float(1674346.56814483)
     assert (m["sourcebus"].positions[0].lat) == float(12272927.0644858)
     assert (m["sourcebus"].positions[0].elevation) == 0
-    assert (m["sourcebus"].feeder_name) == "st_mat_src"
+    assert (m["sourcebus"].feeder_name) == "sourcebus_src"
 
     assert (m["b1"].name) == "b1"
     assert (m["b1"].nominal_voltage) == None
     assert (m["b1"].positions[0].long) == float(1578139)
     assert (m["b1"].positions[0].lat) == float(14291312)
     assert (m["b1"].positions[0].elevation) == 0
-    assert (m["b1"].feeder_name) == "st_mat_src"
+    assert (m["b1"].feeder_name) == "sourcebus_src"
