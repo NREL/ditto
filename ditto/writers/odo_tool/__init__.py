0bbcb11124887754e9014ea65d3e06777c15f5ab
rj <rj@rishabh.jain>
2019-06-28 10:45:54 -0600

    Added parser for 'Operations and Design Optimization (ODO) for Microgrids' >> includes a test example in ditto/tests and documentation in ditto/docs/ODO

---
+++ ditto/writers/odo_tool/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+from builtins import super, range, zip, round, map
+
+from .write import Writer as ODO_writer
+
+__author__ = """Nicolas Gensollen"""
+__email__ = "nicolas.gensollen@nrel.gov"
+__version__ = "0.1.0"
