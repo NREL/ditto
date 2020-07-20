2bc6f524cac60f3779053d79e10153c69ea57140
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-20 17:20:55 -0600

    Change where mdbtools is downloaded from for linux and mac

--- ditto/readers/synergi/utils.py
+++ ditto/readers/synergi/utils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Downloading mdbtools
 import platform
 import os
@@ -12,9 +13,9 @@ mdb_dir = os.path.join(current_dir, "mdbtools")
 if platform.system() == "Windows":
     URL = "https://github.com/kdheepak/mdbtools/releases/download/download/mdbtools-windows.tar.gz"
 elif platform.system() == "Darwin":
-    URL = "https://github.com/kdheepak/mdbtools/releases/download/download/mdbtools-osx.tar.gz"
+    URL = "https://github.com/JuliaBinaryWrappers/mdbtools_jll.jl/releases/download/mdbtools-v0.8.2%2B0/mdbtools.v0.8.2.x86_64-apple-darwin14.tar.gz"
 else:
-    URL = "https://github.com/kdheepak/mdbtools/releases/download/download/mdbtools-linux.tar.gz"
+    URL = "https://github.com/JuliaBinaryWrappers/mdbtools_jll.jl/releases/download/mdbtools-v0.8.2%2B0/mdbtools.v0.8.2.x86_64-linux-musl.tar.gz"
 
 
 def download_mdbtools(url):
