b20c5483889b7ac568971400982b05fa503ed76a
Daniel Thom <daniel.thom@nrel.gov>
2020-08-14 12:48:05 -0600

    Add traitlets version dependency

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     test_suite="tests",
-    install_requires=["click", "future", "networkx", "six", "traitlets", "json_tricks"],
+    install_requires=["click", "future", "networkx", "six", "traitlets>=4.1", "json_tricks"],
     extras_require={
         "all": extras_requires
         + opendss_requires
