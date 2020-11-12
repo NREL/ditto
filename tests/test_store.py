78e36401db112e6afa9a07d77c3b164240cffe46
Daniel Thom <daniel.thom@nrel.gov>
2020-11-12 08:07:42 -0700

    Add tests for exception cases

--- tests/test_store.py
+++ tests/test_store.py
@@ -4,6 +4,7 @@ import os
 import pytest
 
 from ditto.models.line import Line
+from ditto.models.load import Load
 from ditto.models.position import Position
 from ditto.readers.opendss.read import Reader
 from ditto.store import Store, DuplicateNameError, ElementNotFoundError, \
@@ -15,50 +16,66 @@ BUS_COORDS = "tests/data/small_cases/opendss/ieee_13node/buscoord.dss"
 
 
 def test_store():
-    m = Store()
+    model = Store()
     r = Reader(master_file=MASTER, buscoordinates_file=BUS_COORDS)
-    r.parse(m)
+    r.parse(model)
 
     # Test list/iteration variations.
-    lines = m.list_elements(Line)
+    lines = model.list_elements(Line)
     assert len(lines) == 2
     assert isinstance(lines[0], Line)
     line = lines[0]
     name = line.name
 
     found = False
-    for ln in m.iter_elements(Line, lambda x: x.name == name):
+    for ln in model.iter_elements(Line, lambda x: x.name == name):
         assert ln.name == name
 
-    elements = m.list_elements()
+    elements = model.list_elements()
     # There should be more than just lines.
     assert len(elements) > 2
 
     # Test forms of get.
-    assert m.get_element(Line, name) is line
-    assert m[name] is line
+    assert model.get_element(Line, name) is line
+    assert model[name] is line
 
     with pytest.raises(DuplicateNameError):
-        m.add_element(line)
+        model.add_element(line)
 
     with pytest.raises(InvalidElementType):
-        m.add_element("invalid")
+        model.add_element("invalid")
 
     # Position doesn't have a name.
     with pytest.raises(InvalidElementType):
-        m.add_element(Position())
+        model.add_element(Position())
 
-    m.remove_element(line)
+    # Add a Load with the same name.
+    load = Load()
+    load.name = name
+    model.add_element(load)
+    with pytest.raises(DuplicateNameError):
+        model[name]
+
+    # Call get with missing element type
+    for load in model.list_elements(Load):
+        model.remove_element(load)
+    assert not model.list_elements(Load)
     with pytest.raises(ElementNotFoundError):
-        m.get_element(Line, name)
+        model.get_element(Load, name)
+
+    model.remove_element(line)
+    with pytest.raises(ElementNotFoundError):
+        model.get_element(Line, name)
+    with pytest.raises(ElementNotFoundError):
+        model[name]
 
     # Test forms of set.
-    m.add_element(line)
-    assert m.get_element(Line, name) is line
-    m.remove_element(line)
-    m[name] = line
-    assert m.get_element(Line, name) is line
+    model.add_element(line)
+    assert model.get_element(Line, name) is line
+    model.remove_element(line)
+    model[name] = line
+    assert model.get_element(Line, name) is line
 
     # Test clear.
-    m.clear_elements()
-    assert not m.list_elements()
+    model.clear_elements()
+    assert not model.list_elements()
