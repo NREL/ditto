7d968fa08179bd22003304948e6c082d658785a2
Nick Laws <nlaws@camus.energy>
2022-08-26 09:47:11 -0600

    handle negative power factor in cyme reader

--- ditto/readers/cyme/read.py
+++ ditto/readers/cyme/read.py
@@ -5735,9 +5735,9 @@ class Reader(AbstractReader):
                         p, PF = float(settings["value1"]), float(settings["value2"])
                         if PF == 0:
                             q = 0
-                        elif 0 < PF <= 1:
+                        elif 0 < abs(PF) <= 1:
                             q = p * math.sqrt((1 - PF ** 2) / PF ** 2)
-                        elif 1 < PF <= 100:
+                        elif 1 < abs(PF) <= 100:
                             PF /= 100.0
                             q = p * math.sqrt((1 - PF ** 2) / PF ** 2)
                         else:
