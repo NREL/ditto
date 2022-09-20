3ea4d779fff5685243e7ce52204f32c659ab9111
Nick Laws <nlaws@camus.energy>
2022-09-20 17:00:01 -0600

    warn in system_structure_modifier when changing element nominal_voltage

--- ditto/modify/system_structure.py
+++ ditto/modify/system_structure.py
@@ -253,7 +253,8 @@ class system_structure_modifier(Modifier):
         
         if hasattr(self.model[node], "nominal_voltage"):
             if new_value != self.model[node].nominal_voltage:
-                logger.debug(f"Setting node {node} nominal voltage to {new_value} from {self.model[node].nominal_voltage}")
+                if self.model[node].nominal_voltage is not None:
+                    logger.warn(f"Setting node {node} nominal voltage to {new_value} from {self.model[node].nominal_voltage}")
                 self.model[node].nominal_voltage = new_value
         for child in self.G.digraph.successors(node):
             self.set_nominal_voltages_recur(child, new_value, node)
